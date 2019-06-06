67e09c070299caf7b111dc3f7515c922dbd99fd9
Michael <levdevuk@gmail.com>
2019-06-06 15:26:25 +0100

    CMS Preview test

--- src/js/cms-preview-templates/products.js
+++ src/js/cms-preview-templates/products.js
@@ -23,7 +23,7 @@ export default class PostPreview extends React.Component {
 
           <div className="flex-ns flex-wrap mhn2-ns mb3">
             {(entry.getIn(["data", "intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
-              <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
+              <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}} />
               <p>{blurb.get("text")}</p>
             </div>)}
           </div>
@@ -41,59 +41,59 @@ export default class PostPreview extends React.Component {
 
         <div className="flex flex-wrap mhn1">
           <div className="w-100 w-50-ns ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image1", "image"]))}/>
+            <img src={getAsset(entry.getIn(["data", "main", "image1", "image"]))} />
           </div>
 
           <div className="w-100 w-50-ns ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image2", "image"]))}/>
+            <img src={getAsset(entry.getIn(["data", "main", "image2", "image"]))} />
           </div>
 
           <div className="w-100 ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image3", "image"]))}/>
+            <img src={getAsset(entry.getIn(["data", "main", "image3", "image"]))} />
           </div>
         </div>
       </div>
 
       <div className="pb4">
-        {(entry.getIn(['data', 'testimonials']) || []).map((testimonial, index) => <div className="center mb3 ph3" key={index}>
-        	<blockquote className="bg-grey-1 primary pa3 mb3 br1 b mw6 center">
-        		<p className="f4 mb0">“{testimonial.get('quote')}”</p>
-        		<cite className="tr db grey-3">{testimonial.get('author')}</cite>
-        	</blockquote>
+        {(entry.getIn(["data", "testimonials"]) || []).map((testimonial, index) => <div className="center mb3 ph3" key={index}>
+          <blockquote className="bg-grey-1 primary pa3 mb3 br1 b mw6 center">
+            <p className="f4 mb0">“{testimonial.get("quote")}”</p>
+            <cite className="tr db grey-3">{testimonial.get("author")}</cite>
+          </blockquote>
         </div>)}
       </div>
 
-      <img src={getAsset(entry.getIn(['data', 'full_image']))} alt="" className="db w-100"/>
+      <img src={getAsset(entry.getIn(["data", "full_image"]))} alt="" className="db w-100" />
 
       <div className="bg-off-white pv4 ph3">
-      	<div className="mw7 center">
+        <div className="mw7 center">
 
-      		<h2 className="f2 b lh-title mb3">{entry.getIn(['data', 'pricing', 'heading'])}</h2>
-      		<p className="mw6">{entry.getIn(['data', 'pricing', 'description'])}</p>
+          <h2 className="f2 b lh-title mb3">{entry.getIn(["data", "pricing", "heading"])}</h2>
+          <p className="mw6">{entry.getIn(["data", "pricing", "description"])}</p>
 
-      		<div className="flex-ns mhn2-ns mw7">
-            {(entry.getIn(['data', 'pricing', 'plans']) || []).map((plan, index) => <div className="w-33-ns ph2" key={index}>
+          <div className="flex-ns mhn2-ns mw7">
+            {(entry.getIn(["data", "pricing", "plans"]) || []).map((plan, index) => <div className="w-33-ns ph2" key={index}>
               <div className="ph2">
 
-              	<h3 className="b f5 grey-3 tc lh-title mb3">{plan.get('plan')}</h3>
+                <h3 className="b f5 grey-3 tc lh-title mb3">{plan.get("plan")}</h3>
 
-              	<p className="primary f1 b tc lh-title center">
-              		<span className="f4">$</span>{plan.get('price')}
-              	</p>
+                <p className="primary f1 b tc lh-title center">
+                  <span className="f4">$</span>{plan.get("price")}
+                </p>
 
--              	<p className="b">{plan.get('description')}</p>
+                                -              	<p className="b">{plan.get("description")}</p>
 
-              	<ul>
-                  {(plan.get('items') || []).map((item, index) => <li key={index}>
-                    <p className={index + 1 !== plan.get('items').size ? "pb2 mb2 divider-grey" : null}>{item}</p>
+                <ul>
+                  {(plan.get("items") || []).map((item, index) => <li key={index}>
+                    <p className={index + 1 !== plan.get("items").size ? "pb2 mb2 divider-grey" : null}>{item}</p>
                   </li>)}
-              	</ul>
+                </ul>
 
               </div>
 
             </div>)}
-      		</div>
-      	</div>
+          </div>
+        </div>
       </div>
     </div>;
   }
